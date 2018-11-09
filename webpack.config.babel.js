4d875385f5b627511dfb289571e2a4ef67dc4d56
Lee SangHun <dsa28s@naver.com>
2018-11-10 01:48:07 +0900

    Add webpack configuration

---
+++ webpack.config.babel.js
@@ -0,0 +1,52 @@
+import webpack from 'webpack';
+
+export default {
+  entry: {
+    'lib': './src/index.ts',
+    'lib.min': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, '_bundles'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'detect-browser-devtools',
+    umdNamedDefine: true
+},
+resolve: {
+  extensions: ['.ts', '.js']
+},
+devtool: 'source-map',
+    optimization: {
+  minimize: true,
+      runtimeChunk: true,
+      splitChunks: {
+    chunks: "async",
+        minSize: 1000,
+        minChunks: 2,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        name: true,
+        cacheGroups: {
+    default: {
+        minChunks: 1,
+            priority: -20,
+            reuseExistingChunk: true,
+      },
+      vendors: {
+        test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+      }
+    }
+  }
+},
+module: {
+  loaders: [{
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    exclude: /node_modules/,
+    query: {
+      declarations: false,
+    }
+  }]
+}
+}
