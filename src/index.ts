39c091837622c71d31612e658a80e9c0d520d359
Lee SangHun <dsa28s@naver.com>
2018-11-11 16:51:58 +0900

    Add sample source / fix webpack configuration / fix export module

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,46 @@ import { DebuggerManager } from './debugger/DebuggerManager';
 import { ConsoleManager } from './devtools/ConsoleManager';
 import { DevToolsManager } from './devtools/DevToolsManager';
 
-export class DetectBrowserDevTools {
+const devToolsManager = {
   /**
    * If developer tools opened, pause your current browser session.
    * @param isEnabled  `true` is enable this feature.
    */
-  public freezeWhenDevToolsOpened(isEnabled: boolean): void {
+  freezeWhenDevToolsOpened(isEnabled: boolean): void {
     if (isEnabled) {
       DebuggerManager.sharedInstance().startMonitoringDebuggerAttach();
     } else {
       DebuggerManager.sharedInstance().stopMonitoringDebuggerAttach();
     }
-  }
+  },
 
   /**
    * Disable all console log.
    * @param isEnabled  `true` is enable this feature.
    */
-  public alwaysConsoleClear(isEnabled: boolean): void {
+  alwaysConsoleClear(isEnabled: boolean): void {
     if (isEnabled) {
       ConsoleManager.sharedInstance().enableAlwaysClearConsole();
     } else {
       ConsoleManager.sharedInstance().disableAlwaysClearConsole();
     }
-  }
+  },
 
   /**
    * Receive monitoring developer tools isOpened event.
    * @param eventListener  eventListener (isOpened: boolean, orientation: string)
    */
-  public startDevToolMonitoring(eventListener:
-                             (isOpened: boolean, orientation: string) => any): void {
+  startDevToolMonitoring(eventListener:
+    (isOpened: boolean, orientation: string) => any): void {
     DevToolsManager.sharedInstance().startMonitoringDevTools(eventListener);
-  }
+  },
 
   /**
    * Stop monitoring developer tools isOpened event.
    */
-  public stopDevToolMonitoring(): void {
+  stopDevToolMonitoring(): void {
     DevToolsManager.sharedInstance().stopMonitoringDevTools();
-  }
-}
+  },
+};
 
-export default new DetectBrowserDevTools();
+export default devToolsManager;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,26 +18,26 @@ export default {
   devtool: 'source-map',
   optimization: {
     minimize: true,
-    runtimeChunk: true,
-    splitChunks: {
-      chunks: "async",
-      minSize: 1000,
-      minChunks: 2,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      name: true,
-      cacheGroups: {
-        default: {
-          minChunks: 1,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        }
-      }
-    }
+    // runtimeChunk: true,
+    // splitChunks: {
+    //   chunks: "async",
+    //   minSize: 1000,
+    //   minChunks: 2,
+    //   maxAsyncRequests: 5,
+    //   maxInitialRequests: 3,
+    //   name: true,
+    //   cacheGroups: {
+    //     default: {
+    //       minChunks: 1,
+    //       priority: -20,
+    //       reuseExistingChunk: true,
+    //     },
+    //     vendors: {
+    //       test: /[\\/]node_modules[\\/]/,
+    //       priority: -10,
+    //     }
+    //   }
+    // }
   },
   module: {
     rules: [
